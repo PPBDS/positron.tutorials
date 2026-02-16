dbc7187412269f32f949ea1679885dcdfa32b632
David Kane <dave.kane@gmail.com>
2026-02-16 17:52:25 -0500

    solve the  data-too-big--to-fit-on-CRAN problem

--- R/zzz.R
+++ R/zzz.R
@@ -1,29 +1,81 @@
-# R/zzz.R
+# The data files used by several tutorials (r4ds-1 through r4ds-5,
+# tidycensus-1) are too large to include in the CRAN package. However, when a
+# tutorial is run via learnr::run_tutorial(), the working directory is set to
+# the directory containing the tutorial.Rmd file. Code like
+# open_dataset("data/game.parquet") works because it resolves relative to that
+# directory. So the data files must exist in each tutorial's data/ subdirectory
+# within the installed package.
+#
+# When installing from GitHub, the data files are included in inst/tutorials/
+# and get installed normally. But the CRAN version ships without them. The
+# .onAttach() hook below checks for missing data files on package load and
+# downloads them from GitHub directly into the installed package directory. This
+# is safe because if a user has permission to install the package, they have
+# permission to write into its directory. The manifest below must be updated
+# whenever data files are added or removed from any tutorial.
+
+data_manifest <- list(
+  "r4ds-1" = c("music_1.csv", "music_fake.csv", "music.csv"),
+  "r4ds-2" = c("cheeses.xlsx", "wine.xlsx"),
+  "r4ds-3" = c("nameby_year.duckdb", "nycflights13.duckdb"),
+  "r4ds-4" = c("fifa.parquet", "game.parquet", "line_score.parquet"),
+  "r4ds-5" = c("earthquakes.geojson"),
+  "tidycensus-1" = c("age_ca.rds", "edu_ca.rds", "income_tx.rds")
+)
+
 .onAttach <- function(libname, pkgname) {
-  tutorials_path <- system.file("tutorials", package = "positron.tutorials")
   
-  # Count tutorials with data directories
-  tutorials_with_data <- 0
+  base_url <- "https://raw.githubusercontent.com/PPBDS/positron.tutorials/main/inst/tutorials"
+  files_downloaded <- character(0)
   
-  if (dir.exists(tutorials_path)) {
-    tutorial_dirs <- list.dirs(tutorials_path, full.names = FALSE, recursive = FALSE)
+  for (tut in names(data_manifest)) {
     
-    for (tutorial in tutorial_dirs) {
-      data_dir <- file.path(tutorials_path, tutorial, "data")
-      if (dir.exists(data_dir)) {
-        tutorials_with_data <- tutorials_with_data + 1
+    local_tut_dir <- system.file("tutorials", tut, package = pkgname)
+    
+    # Skip tutorials not in the installed version
+    if (!nzchar(local_tut_dir)) next
+    
+    local_data_dir <- file.path(local_tut_dir, "data")
+    
+    # Check which files are missing
+    if (dir.exists(local_data_dir)) {
+      local_files <- list.files(local_data_dir)
+      missing <- setdiff(data_manifest[[tut]], local_files)
+    } else {
+      missing <- data_manifest[[tut]]
+    }
+    
+    if (length(missing) == 0) next
+    
+    # Create data directory if needed
+    if (!dir.exists(local_data_dir)) {
+      dir.create(local_data_dir, recursive = TRUE)
+    }
+    
+    # Download missing files
+    for (f in missing) {
+      dest <- file.path(local_data_dir, f)
+      result <- tryCatch({
+        download.file(
+          paste0(base_url, "/", tut, "/data/", f),
+          dest,
+          mode = "wb",
+          quiet = TRUE
+        )
+        TRUE
+      }, error = function(e) FALSE)
+      
+      if (result) {
+        files_downloaded <- c(files_downloaded, file.path(tut, "data", f))
       }
     }
   }
   
-  # Only show message if fewer than 3 tutorials have data
-  if (tutorials_with_data < 3) {
+  # Only print if we downloaded something
+  if (length(files_downloaded) > 0) {
     packageStartupMessage(
-      "It looks like you don't have the data necessary for running the tutorials in this package. ",
-      "To get that data, run `setup_tutorial_data()` from the Console. ",
-      "Then, restart R. If the tutorials don't work after you do this, ",
-      "then your best bet is to install the development version of the package by running ",
-      "`pak::pak(\"PPBDS/positron.tutorials\")` at the Console and then restart R."
+      "positron.tutorials: Downloaded ", length(files_downloaded), " data file(s):\n",
+      paste0("  ", files_downloaded, collapse = "\n")
     )
   }
 }
